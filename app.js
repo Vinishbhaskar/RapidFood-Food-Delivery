d1ce057ed9794d49b94b4bca222efd2ce85a2f64
Vinish Bhaskar <vinish.bhaskar@outlook.com>
2023-02-08 21:01:21 +0530

    app.js modified

--- app.js
+++ app.js
@@ -1,44 +1,25 @@
 import React from "react"
 import ReactDOM  from "react-dom/client"
 
-const heading1 = React.createElement("h2",{
-    id:"title",
-    style:{
-        width:"auto",
-        height:"auto",
-        position:"relative",
-        margin:"0px",
-        backgroundColor: "black",
-        color: "red",
-        padding: "20px",
-        // border:"5px dotted blue",
-        borderBottom:"5px solid orange",
-        fontFamily: "roboto",
-        textAlign: "center",
-        // textDecoration: "underline",
-        // textDecorationColor: "white"
-    }
-},"Learn React Creatively")
+const title =()=> (
+    <h1>Title Invoked</h1>
+);
 
-const list1 = React.createElement("li",{
-    id:"list"
-},"List 1")
+const HeaderComponent =() =>  {
+    return (
+        <div>
+            {title()}
+            <h1>Learning React</h1>
+            <h2>Creating Heading </h2>
+            <ol className="ol">
+            <li>Item 1</li>
+            <li>Item 2</li>
+            <li>Item 3</li>
+            </ol>
+        </div>
+    )
+};
 
-const list2 = React.createElement("li",{
-    id:"list"
-},"List 2")
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-const list3 = React.createElement("li",{
-    id:"list"
-},"List 3")
-
-
-
-const heading = React.createElement("div",{
-    id:"container",
-},[heading1, list1,list2,list3])
-console.log(heading)
-
-const root =  ReactDOM.createRoot(document.getElementById("root"))
-
-root.render(heading)
+root.render(<HeaderComponent/>)
