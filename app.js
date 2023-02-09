ab515f4ce3020e0bdb0f1939fec264a1adef09b1
Vinish Bhaskar <vinish.bhaskar@outlook.com>
2023-02-09 21:32:36 +0530

    App Layput

--- app.js
+++ app.js
@@ -1,25 +1,14 @@
 import React from "react"
 import ReactDOM  from "react-dom/client"
 
-const title =()=> (
-    <h1>Title Invoked</h1>
-);
-
-const HeaderComponent =() =>  {
+const AppLayout =() =>  {
     return (
-        <div>
-            {title()}
-            <h1>Learning React</h1>
-            <h2>Creating Heading </h2>
-            <ol className="ol">
-            <li>Item 1</li>
-            <li>Item 2</li>
-            <li>Item 3</li>
-            </ol>
-        </div>
+        <>
+            
+        </>
     )
 };
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<HeaderComponent/>)
+root.render(<AppLayout/>)
