452ad43a9a094c6bd3c10ee87b5b1fbfb5bd8d64
Vinish Bhaskar <vinish.bhaskar@outlook.com>
2023-02-09 21:36:50 +0530

    Food Delivery App Layout

--- app.js
+++ app.js
@@ -1,10 +1,32 @@
 import React from "react"
 import ReactDOM  from "react-dom/client"
 
+const Header =()=> {
+    return (
+        <h1> Food Delivery App</h1>
+    )
+}
+
+const MainBody =()=> {
+    return (
+        <h1> Food Delivery App</h1>
+    )
+}
+
+const Footer =()=> {
+    return (
+        <h1> Footer </h1>
+    )
+}
+
+
+
 const AppLayout =() =>  {
     return (
         <>
-            
+            <Header/>
+            <MainBody />
+            <Footer />
         </>
     )
 };
